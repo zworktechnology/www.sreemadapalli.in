49ec462a8767a33f16649aaaac60f63d4aac1c77
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-05-26 13:43:52 +0530

    Main

--- resources/views/pages/backend/sales/addon/deliveryby.blade.php
+++ resources/views/pages/backend/sales/addon/deliveryby.blade.php
@@ -12,10 +12,10 @@
                     <th><img src="{{ asset('assets/images/cash.jpg') }}" style="width: 15px; height: 15px;"/> Cash</th>
                     <th><img src="{{ asset('assets/images/pending.png') }}" style="width: 15px; height: 15px;"/> Pending</th>
                     <th><img src="{{ asset('assets/images/gpay.png') }}" style="width: 35px; height: 15px;"/></th>
-                    <th><img src="{{ asset('assets/images/gpayb.png') }}" style="width: 20px; height: 20px;"/></th>
-                    <th><img src="{{ asset('assets/images/phonepay.png') }}" style="width: 20px; height: 20px;"/></th>
-                    <th><img src="{{ asset('assets/images/paytm.png') }}" style="width: 35px; height: 20px;"/></th>
-                    <th><img src="{{ asset('assets/images/card.png') }}" style="width: 20px; height: 20px;"/></th>
+                    {{-- <th><img src="{{ asset('assets/images/gpayb.png') }}" style="width: 20px; height: 20px;"/></th> --}}
+                    {{-- <th><img src="{{ asset('assets/images/phonepay.png') }}" style="width: 20px; height: 20px;"/></th> --}}
+                    {{-- <th><img src="{{ asset('assets/images/paytm.png') }}" style="width: 35px; height: 20px;"/></th> --}}
+                    {{-- <th><img src="{{ asset('assets/images/card.png') }}" style="width: 20px; height: 20px;"/></th> --}}
                 </tr>
             </thead>
             <tbody id="">
@@ -27,10 +27,10 @@
                     <td>{{ $deliveryboys_array['cash'] }}</td>
                     <td>{{ $deliveryboys_array['pending'] }}</td>
                     <td>{{ $deliveryboys_array['gpay'] }}</td>
-                    <td>{{ $deliveryboys_array['gpaybusiness'] }}</td>
-                    <td>{{ $deliveryboys_array['phonepe'] }}</td>
-                    <td>{{ $deliveryboys_array['paytm'] }}</td>
-                    <td>{{ $deliveryboys_array['card'] }}</td>
+                    {{-- <td>{{ $deliveryboys_array['gpaybusiness'] }}</td> --}}
+                    {{-- <td>{{ $deliveryboys_array['phonepe'] }}</td> --}}
+                    {{-- <td>{{ $deliveryboys_array['paytm'] }}</td> --}}
+                    {{-- <td>{{ $deliveryboys_array['card'] }}</td> --}}
                 </tr>
                 @endforeach
 
